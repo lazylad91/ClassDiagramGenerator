ec99cfd4e762479b4eedcb4b6a8a2d64a5f80972
lazylad91 <parteek.makkar@sjsu.edu>
2015-12-21 22:10:29 -0800

    Class Diagram Generator

---
+++ src/main/java/StringLiteral.java
@@ -0,0 +1,12 @@
+
+public class StringLiteral {
+
+	public static void main(String[] args) {
+	String s1="hi";
+	String s2="hi";
+	String s3 = new String("hi");
+	String s4 = new String("hi");
+	System.out.println(s4.equals(s3));
+	}
+
+}
